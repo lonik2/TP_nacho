4c7a6000877b16699eafc9834d3a69f5f7835fbc
Lucas <lonyszczyk@alumno.huergo.edu.ar>
2026-04-09 13:13:28 -0300

    archivos

---
+++ tp.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("Reproductor de musica")
+root.geometry("300x400")
+root.configure(bg="#2C2F33")
+
+style = ttk.Style()
+style.configure("TButton", font=("Arial", 12), padding=10)
